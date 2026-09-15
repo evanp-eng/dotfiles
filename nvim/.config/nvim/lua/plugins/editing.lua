500d84c14aa041227a247040c1df8e6410c7f5f6
evanp-eng <evanp@ulysses.eco>
2026-09-15 15:06:33 -0700

    adding hotkeys to match some vs code shortcuts

--- nvim/.config/nvim/lua/plugins/editing.lua
+++ nvim/.config/nvim/lua/plugins/editing.lua
@@ -1,6 +1,21 @@
 return {
   { "windwp/nvim-autopairs", event = "InsertEnter", opts = {} },
-  { "numToStr/Comment.nvim", event = "VeryLazy", opts = {} },
+  {
+    "numToStr/Comment.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("Comment").setup()
+      local api = require("Comment.api")
+      local visual_toggle = function()
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "nx", false)
+        api.toggle.linewise(vim.fn.visualmode())
+      end
+      for _, key in ipairs({ "<C-_>", "<C-/>" }) do
+        vim.keymap.set("n", key, api.toggle.linewise.current, { desc = "Toggle comment" })
+        vim.keymap.set("x", key, visual_toggle, { desc = "Toggle comment" })
+      end
+    end,
+  },
   { "folke/which-key.nvim", event = "VeryLazy", opts = {} },
   { "christoomey/vim-tmux-navigator", lazy = false },
   {
@@ -15,5 +30,12 @@ return {
         typescript = { "prettier" },
       },
     },
+  },
+  {
+    "mg979/vim-visual-multi",
+    branch = "master",
+    init = function()
+      vim.g.VM_maps = { ["Find Under"] = "<C-d>", ["Find Subword Under"] = "<C-d>" }
+    end,
   },
 }
